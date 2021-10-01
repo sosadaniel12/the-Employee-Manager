f8a05524d50c60419a36da017473d8597e66afa7
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-10-01 15:30:48 -0500

    imported the employee into the index.js

--- js/employee.js
+++ js/employee.js
@@ -1,15 +1,27 @@
+//Create the employee class
 class Employee {
-  constructor(name, id, email) {
+  constructor(name, id, email, role) {
     this.name = name;
     this.id = id;
     this.email = email;
+    this.role = role;
   }
+  //Create the getName() method for the employee class
   getName(employeeName) {
     this.name = employeeName;
   }
-  getId() {}
-  getEmail() {}
-  getRole() {}
+  //Create the getId() method for the employee class
+  getId(employeeId) {
+    this.id = employeeId;
+  }
+  //Create the getEmail() method for the employee class
+  getEmail(employeeEmail) {
+    this.email = employeeEmail;
+  }
+  //Create the getRole() method for the employee class
+  getRole(employeeRole) {
+    this.role = employeeRole;
+  }
 }
 
 module.exports = Employee;
---
+++ js/index.js
@@ -0,0 +1,2 @@
+//Get all the requires from the other files
+const Employee = require("./employee");
